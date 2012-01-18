e87e8f35328f66caac534d43b618b328ba4df2a1
Jakub Kovar <jakub.kovar@smartmedia.cz>
2012-01-18 09:47:21 +0100

    partial search

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
 
 	# Display search results given the query supplied
   def show
-    @results = SearchEngine.search(params[:query], params[:page])
+    @results = SearchEngine.search('^'+params[:query].to_s+'^', params[:page])
 
     present(@page = Page.find_by_link_url("/search"))
   end
